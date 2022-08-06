523a297166d59076e8ed07675d780c499391d09b
Mauricio Burgos Herrera <mbh@patientsky.com>
2022-08-06 13:35:08 +0200

    Add homebrew to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+fish_add_path /opt/homebrew/bin
+
+[ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
